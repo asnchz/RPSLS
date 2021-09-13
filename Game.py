e24b6f61c344f6d0ad8599927fb81da659844798
unknown <asnchz27@yahoo.com>
2021-09-13 09:51:18 -0500

    game intro and player class

---
+++ Game.py
@@ -0,0 +1,18 @@
+class Game:
+    def __init__(self):
+        pass
+
+    def game_intro(self):
+        print("Welcome to RPSLS!")
+        print("The rules are as follows: each player will pick one of gestures.")
+        print("Gesture options are: Rock, Paper, Scissors, Lizard, or spock.")
+        print("Rock crushes Scissors")
+        print("Scissors cuts Paper")
+        print("Paper covers Rock")
+        print("Rock crushes Lizard")
+        print("Lizard poisons Spock")
+        print("Spock smashes Scissors")
+        print("Scissors decapitates Lizard")
+        print("Lizard eats Paper")
+        print("Paper disproves Spock")
+        print("Spock vaporizes Rock")
