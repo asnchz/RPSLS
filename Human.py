d0fcf6e580a660d2d1289aff83d5cd724d547c49
unknown <asnchz27@yahoo.com>
2021-09-13 16:21:46 -0500

    human gesture

--- Human.py
+++ Human.py
@@ -5,4 +5,13 @@ class Human(Player):
         super().__init__()
 
     def choose_gesture(self):
-        pass
+        print(self.gestures)
+        player_gesture = input("Pick your gesture")
+        for gesture in self.gestures:
+            if player_gesture == gesture:
+                self.chosen_gesture = player_gesture
+                return self.chosen_gesture
+
+        else:
+            print("Not a valid gesture.. try again")
+            self.choose_gesture()
